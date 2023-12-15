147f607951dc064d88f87988474d0f73b7106ca2
ZuzanaS <zuzana.matejovska@gmail.com>
2023-12-15 07:59:44 +0100

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,7 +1,7 @@
 SELECT
 tzp.year_price,
-SUM (tzp.percent_payroll),
-SUM (tzp.percent_avg_price),
+tzp.percent_payroll,
+tzp.percent_avg_price,
 tzp.percent_payroll - tzp.percent_avg_price AS increase
 FROM t_zuzana_salamon_project_SQL_primary_final tzp
 WHERE tzp.percent_payroll > 0
