3a88883f05fedc65821bb852e74654a76d2be705
ZuzanaS <zuzana.matejovska@gmail.com>
2023-12-15 10:18:59 +0100

    Update Q5.sql

--- Q5.sql
+++ Q5.sql
@@ -1,36 +1,12 @@
 SELECT DISTINCT 
 year_price AS common_year,
-name_industry,
+avg_price,
 percent_payroll,
+percent_avg_price,
+previous_percent_diff_GDP,
 percent_diff_GDP 
 FROM t_zuzana_salamon_project_SQL_primary_final tzspspf 
 WHERE name_industry IS NULL 
-ORDER BY percent_diff_GDP DESC;
-
-SELECT DISTINCT 
-year_price AS common_year,
-name_industry,
-percent_payroll, 
-previous_percent_diff_GDP
-FROM t_zuzana_salamon_project_SQL_primary_final tzspspf 
-WHERE name_industry IS NULL 
-ORDER BY previous_percent_diff_GDP DESC;
-
-
-SELECT DISTINCT 
-year_price AS common_year,
-product_name,
-percent_avg_price,
-percent_diff_GDP
-FROM t_zuzana_salamon_project_SQL_primary_final tzspspf  
-WHERE percent_avg_price  IS NOT NULL
-ORDER BY percent_diff_GDP DESC;
-
-SELECT DISTINCT 
-year_price AS common_year,
-product_name,
-percent_avg_price,
-previous_percent_diff_GDP 
-FROM t_zuzana_salamon_project_SQL_primary_final tzspspf  
-WHERE percent_avg_price  IS NOT NULL
-ORDER BY previous_percent_diff_GDP DESC;
+	OR avg_price IS NULL
+GROUP BY year_price 
+ORDER BY common_year;
