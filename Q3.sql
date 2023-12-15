e7bce6df595932bd3de670f27a688d4c7effd437
ZuzanaS <zuzana.matejovska@gmail.com>
2023-12-15 07:32:19 +0100

    Update Q3.sql

--- Q3.sql
+++ Q3.sql
@@ -6,7 +6,7 @@ last_year.avg_price AS year_2018,
 ((last_year.avg_price - first_year.avg_price)/first_year.avg_price)*100 AS percent_price_difference
 FROM t_zuzana_salamon_project_SQL_primary_final first_year
 JOIN t_zuzana_salamon_project_SQL_primary_final last_year ON last_year.product_name = first_year.product_name 
-AND last_year.year_price = 2018  
+	AND last_year.year_price = 2018  
 WHERE first_year.product_name IS NOT NULL   
 	AND first_year.year_price = 2006
 ORDER BY percent_price_difference;
