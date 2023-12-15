e72661a4bce2f244c608fee68e273337406194c8
ZuzanaS <zuzana.matejovska@gmail.com>
2023-12-15 09:46:59 +0100

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,11 +1,11 @@
-SELECT
+SELECT DISTINCT 
 tzp.year_price,
 tzp.percent_payroll,
 tzp.percent_avg_price,
-tzp.percent_payroll - tzp.percent_avg_price AS increase
+tzp.percent_avg_price - tzp.percent_payroll AS increase
 FROM t_zuzana_salamon_project_SQL_primary_final tzp
 WHERE tzp.percent_payroll > 0
 	AND tzp.percent_avg_price > 0
-	AND (tzp.percent_payroll - tzp.percent_avg_price) > 10
+	AND (tzp.percent_avg_price - tzp.percent_payroll) > 10
 GROUP BY tzp.year_price 
 ORDER BY increase;
