4dfae9eb30c8e35c3c8b17ab2e2966bdca90c74e
ZuzanaS <zuzana.matejovska@gmail.com>
2023-12-18 10:06:31 +0100

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,8 +1,8 @@
 SELECT DISTINCT
 tzp.year_price,
-avg (tzp.percent_avg_price),
-avg (tzp.percent_payroll),
-avg (tzp.percent_avg_price) - avg (tzp.percent_payroll) AS increase
+AVG(tzp.percent_avg_price) AS avg_percent_avg_price,
+AVG(tzp.percent_payroll) AS avg_percent_payroll,
+AVG(tzp.percent_avg_price) - AVG(tzp.percent_payroll) AS increase
 FROM t_zuzana_salamon_project_SQL_primary_final tzp
 WHERE tzp.name_industry IS NULL
 GROUP BY tzp.year_price
